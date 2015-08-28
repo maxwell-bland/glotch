b76e3673a2a3e924d528f3d7d3919be420a9dd09
Maxwell Bland <maxwell.t.bland@gmail.com>
2015-08-28 13:41:31 -0700

    Added undo option

--- imagewindow.py
+++ imagewindow.py
@@ -2,8 +2,12 @@ import Tkinter
 from PIL import Image, ImageTk
 import random
 
+
+imagelist = []
+operationlist = []
 numdegrades = 7
-im = Image.open(raw_input("what is your command master? "))
+picname = raw_input("what is your command master? ")
+im = Image.open(picname)
 root = Tkinter.Tk()  
 tkimage = ImageTk.PhotoImage(im)
 canvas = Tkinter.Label(root, image=tkimage)
@@ -14,7 +18,7 @@ def shiftover(image):
     (width, height) = image.size
     start = 0
     end = random.randint(0,height)
-    while end <= height:
+    while end < height:
         if random.random() > .5:
             randheight = random.randint(0,height/6)
             randwidth = random.randint(0,width/2)
@@ -33,14 +37,28 @@ def shiftover(image):
                     image.putpixel((x + randwidth,y), (r,g,b))
             start += end
             end += randheight
+        if end >= height:
+            end = height
+            if random.random() > .5:
+                randheight = random.randint(0,height/6)
+                randwidth = random.randint(0,width/2)
+                for x in xrange(randwidth, width):
+                    for y in xrange(start, end):
+                        (r,g,b) = image.getpixel((x,y))
+                        image.putpixel((x-randwidth,y), (r,g,b))
+                start += end
+                end += randheight
+            else:
+                randheight = random.randint(0,height)
+                randwidth = random.randint(0,width/2)
+                for x in xrange(0, randwidth):
+                    for y in xrange(start, end):
+                        (r,g,b) = image.getpixel((x,y))
+                        image.putpixel((x + randwidth,y), (r,g,b))
+                start += end
+                end += randheight
     return image
 
-
-def shifter():
-    im2 = shiftover(im)
-    tkimage2 = ImageTk.PhotoImage(im2)
-    canvas.configure(image = tkimage2)
-    canvas.image = tkimage2
     
 
 def shiftNBitsTo8( num, N ):
@@ -64,25 +82,13 @@ def degradeColors( pic, N):
       picCopy.putpixel( (x,y), ( r, g, b ) )
   return picCopy
 
-def degrader():
-    global numdegrades
-    global im
-    if numdegrades > 4:
-        print numdegrades
-        im2 = degradeColors(im, numdegrades)
-        im = im2
-        tkimage2 = ImageTk.PhotoImage(im2)
-        canvas.configure(image = tkimage2)
-        canvas.image = tkimage2
-        numdegrades = numdegrades - 1
-
 
 def tearover(image):
     """ Copies the left half of the picture to the right half"""
     (width, height) = image.size
     start = 0
     end = random.randint(0,height)
-    while end <= height:
+    while end < height:
         if random.random() > .5:
             randheight = random.randint(0,height/6)
             randwidth = random.randint(0,width/2)
@@ -103,17 +109,85 @@ def tearover(image):
                     image.putpixel((x + randwidth,y), (r,g,b))
             start += end
             end += randheight
+        if end >= height:
+            end = height
+            if random.random() > .5:
+                randheight = random.randint(0,height/6)
+                randwidth = random.randint(0,width/2)
+                for x in xrange(randwidth, width,2):
+                    for y in xrange(start, end,2):
+                        (r,g,b) = image.getpixel((x,y))
+                        r = int(r * 4)
+                        image.putpixel((x-randwidth,y), (r,g,b))
+                start += end
+                end += randheight
+            else:
+                randheight = random.randint(0,height)
+                randwidth = random.randint(0,width/2)
+                for x in xrange(0, randwidth,2):
+                    for y in xrange(start, end,2):
+                        (r,g,b) = image.getpixel((x,y))
+                        b = int(b * 4)
+                        image.putpixel((x + randwidth,y), (r,g,b))
+                start += end
+                end += randheight
+            break
     return image
+
+def shifter():
+    im2 = shiftover(im)
+    tkimage2 = ImageTk.PhotoImage(im2)
+    canvas.configure(image = tkimage2)
+    canvas.image = tkimage2
+    imagecopy = im2.copy()
+    imagelist.append(imagecopy)
+    operationlist.append('shifter')
+
+def degrader():
+    global numdegrades
+    global im
+    if numdegrades > 4:
+        im2 = degradeColors(im, numdegrades)
+        im = im2
+        tkimage2 = ImageTk.PhotoImage(im2)
+        canvas.configure(image = tkimage2)
+        canvas.image = tkimage2
+        numdegrades = numdegrades - 1
+        imagecopy = im2.copy()
+        imagelist.append(imagecopy)
+        operationlist.append('degrader')
 
 def tear():
     im2 = tearover(im)
     tkimage2 = ImageTk.PhotoImage(im2)
     canvas.configure(image = tkimage2)
     canvas.image = tkimage2
-    
-Tkinter.Button(text="Shift Me", fg="black",command = shifter).pack(side = 'left')
-Tkinter.Button(text="Degrade Me", fg="black", command = degrader).pack(side = 'right')
-Tkinter.Button(text="Tear Me", fg="black", command = tear).pack(side = 'left')
+    imagecopy = im2.copy()
+    imagelist.append(imagecopy)
+    operationlist.append('tear')
+
+def undo():
+    global im
+    global numdegrades
+    if len(imagelist) > 0:
+        lastoper = operationlist.pop()
+        if lastoper == 'degrader':
+            numdegrades += 1
+        im = imagelist[-1]
+        im2 = imagelist.pop()
+        tkimage2 = ImageTk.PhotoImage(im2)
+        canvas.configure(image = tkimage2)
+        canvas.image = tkimage2
+    else:
+        im = Image.open(picname)
+        tkimage = ImageTk.PhotoImage(im)
+        canvas.configure(image = tkimage)
+        canvas.image = tkimage
+        
+Tkinter.Button(text="Shift", fg="black",command = shifter).pack(side = 'left')
+Tkinter.Button(text="Undo", fg="black", command = undo).pack(side = 'right')
+Tkinter.Button(text="Degrade", fg="black", command = degrader).pack(side = 'right')
+Tkinter.Button(text="Tear", fg="black", command = tear).pack(side = 'left')
 root.mainloop()
 
 
